85ee84505cf83150a41aa84c5b221805515dbbed
VincentS972 <vincentsechi@gmail.com>
2023-07-18 20:20:05 -0400

    Added SignUp Page route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from './Components/NavBar';
 import Home from "./Components/Home"
 import SignIn from './Components/SignIn';
+import SignUp from "./Components/Signup";
 import Forum from "./Components/Forum";
 import Profile from "./Components/Profile";
 import Footer from "./Components/Footer";
@@ -20,6 +21,7 @@ function App() {
           <Route path="/Profile/:id" element={<Profile />} />
           <Route path="/SignIn" element={<SignIn />} />
           <Route path="/Profile/update/:id" element={<UpdateProfile/>}/>
+          <Route path="/SignIn/SignUp" element={<SignUp />}/>
         </Routes>
         <Footer/>
       </Router>
